60e5c5b06b7eca0286546d5b6a695907808dd9a7
Flappy <stvnsbarrantes@gmail.com>
2025-08-12 16:38:15 +0800

    Revert "Feat: Show data"
    This reverts commit 57148cfe8f78a54485074366435d9ddbdc8c9566.

--- app/shared/Date.ts
+++
@@ -1,8 +0,0 @@
-export function dateConvert(dates: string) {
-	const date = new Date(dates)
-	return date.toLocaleDateString('en-US', {
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-	})
-}
